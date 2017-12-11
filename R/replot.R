e9e96556fce70b3e6436298e897fea17fc64cb48
Alexander Zwart <zwa005@csiro.au>
2017-12-11 13:15:16 +1100

    Update plotting colours

--- R/replot.R
+++ R/replot.R
@@ -1,14 +1,11 @@
 
-## Hex #DBD100 is an orange colour that may suffice for all viewers as
-## a better substitute for the green I am using...
-
-
 ## Function replot
 ##
-##' Plot the random effects parameter estimates from one or two fitted
-##' \code{\link{asreml}} models.  If two models (termed the 'current' and
-##' 'previous' models) are specified, the plot compares the parameter
-##' estimates from the current model to those from the previous.
+##' Function \code{replot} plots the random effects parameter
+##' estimates from one or two fitted \code{\link{asreml}} models.  If
+##' two models (termed the 'current' and 'previous' models) are
+##' specified, the plot compares the parameter estimates from the
+##' current model to those from the previous.
 ##'
 ##' Function \code{replot} plots the random effects parameter
 ##' estimates from one or two fitted \code{\link{asreml}} model
@@ -17,23 +14,24 @@
 ##' by \code{\link{summary.asreml}}.  'Gamma' values (see below), or
 ##' unscaled parameter values can be plotted.  Residual variance
 ##' parameters are highlighted on the plot via vertical dashed lines
-##' when \code{rlines} is TRUE (the default), and the position of the
-##' origin is highlighted by a vertical grey line when \code{zline} is
-##' TRUE (the default).
+##' when \code{rlines} is \code{TRUE} (the default), and the position
+##' of the origin is highlighted by a vertical grey line when
+##' \code{zline} is \code{TRUE} (the default).
 ##'
 ##' Regarding 'gamma' values: when a linear mixed model contains a
 ##' single residual variance \code{\link{asreml}} will parameterise
-##' other variances in the model relative to the residual variance.
-##' These scaled variances are referred to as 'gamma' parameters.  The
-##' \code{varcomp} component of the object returned by
-##' \code{\link{summary.asreml}} reports these gamma values, along
+##' other variances in the model \emph{relative} to the residual
+##' variance.  The resulting scale factors are referred to as 'gamma'
+##' parameters.  The \code{varcomp} component of the object returned
+##' by \code{\link{summary.asreml}} reports these gamma values, along
 ##' with the unscaled component values, and \code{replot} allows you
-##' to plot either via the argument \code{gammas} (default value
-##' TRUE).  No such scaling is performed when a model contains more
-##' than one residual variance parameter, and the reported gamma
-##' estimates are identical to the unscaled component estimates.  For
-##' further details on how \code{\link{asreml}} parameterises the
-##' linear mixed model, see the \pkg{asreml} package manual.
+##' to plot either, via the argument \code{gammas} (default value
+##' \code{TRUE}).  Note that no such scaling is performed when a model
+##' contains more than one residual variance parameter, and the
+##' reported gamma estimates are identical to the unscaled component
+##' estimates.  For further details on how \code{\link{asreml}}
+##' parameterises the linear mixed model, see the \pkg{asreml} package
+##' manual.
 ##'
 ##' Parameters that lie 'on the boundary' (and hence indicated by
 ##' "Boundary" in the \code{varcomp$condition} component of the
@@ -53,17 +51,18 @@
 ##'
 ##' For parameters present in both models, the values for the current
 ##' model are plotted, and horizontal line segments are used to
-##' indicate how these parameters have changed between the previous and
-##' current models.  Note that if parameter changes are very small,
-##' the line segments may be too short to be seen.
+##' indicate how these parameters have changed between the previous
+##' and current models.  Note that these line segments are plotted
+##' \emph{behind} the corresponding points, so if parameter changes
+##' are very small, the line segments may be too short to be seen.
 ##'
 ##' Caveat: note that a change in the random effects model
 ##' specification between 'previous' and 'current' models, can
-##' sometimes result in an equivalent parameter being given different
-##' labels by \code{\link{asreml}} between the models.  \code{replot}
-##' cannot detect such cases, and hence will plot the affected
-##' parameter as two distinct parameters - one removed from the
-##' previous model, the other added to the current.
+##' sometimes result in an equivalent parameter being given
+##' \emph{different labels} by \code{\link{asreml}} between the two
+##' models.  \code{replot} cannot detect such cases, and hence will
+##' plot the affected parameter as two distinct parameters - one
+##' removed from the previous model, the other added to the current.
 ##'
 ##' Not all random effects parameters are variances, and as such may
 ##' have different scales to the variances.  Autocorrelation
@@ -75,8 +74,8 @@
 ##' \pkg{ggplot2} \code{xlim} function syntax,
 ##' e.g. \code{replot(a1,a0) + xlim(-1,1)}.
 ##'
-##' \code{replot} requires that the \pkg{dplyr} and \pkg{ggplot2}
-##' packages be installed.  \code{replot} returns a
+##' \code{replot} requires that the \pkg{asreml}, \pkg{dplyr} and
+##' \pkg{ggplot2} packages be installed.  \code{replot} returns a
 ##' \code{\link{ggplot}} object, and this object can be further
 ##' modified using the usual \pkg{ggplot2} package
 ##' syntax. \code{replot} uses the \code{\link{theme_bw}} theme by
@@ -86,11 +85,11 @@
 ##' @param curr \code{\link{asreml}} object: The 'current'
 ##' \code{\link{asreml}} model for which the estimated random effects
 ##' parameters are to be plotted.
-##' @param prev \code{\link{asreml}} object, or NULL: If a 'previous'
-##' \code{\link{asreml}} model is specified via \code{prev}, the plot
-##' will compare random effects parameters between the previous and
-##' current models.  Default is \code{NULL}.  See the Details section
-##' for more.
+##' @param prev \code{\link{asreml}} object, or \code{NULL}: If a
+##' 'previous' \code{\link{asreml}} model is specified via
+##' \code{prev}, the plot will compare random effects parameters
+##' between the previous and current models.  Default is \code{NULL}.
+##' See the Details section for more.
 ##' @param gammas logical: Should the plot display 'gamma' values or
 ##' actual components?  Default is \code{TRUE}. See the Details
 ##' section for more.
@@ -101,24 +100,34 @@
 ##' indicating changes in parameters common to the current and
 ##' previous models.  Equivalent to the \code{\link{geom_segment}}
 ##' '\code{size}' parameter.
-##' @param tritanopia logical: The default colors used by
-##' \code{replot} should (I hope!) be protanopia- and
-##' deuteranopia-friendly. Tritanopia-friendly colors can be obtained
-##' by setting \code{tritanopia=TRUE}.
 ##' @param zline logical: Display a grey vertical line (the 'zero
-##' line') passing through the x-axis origin? Default is TRUE.
+##' line') passing through the x-axis origin? Default is \code{TRUE}.
 ##' @param rlines logical: Display dashed vertical line(s) passing
 ##' through the residual variance parameter estimate(s)? Default is
-##' TRUE.
+##' \code{TRUE}.
+##' @param bw logical: Produce a black-and-white version of the plot?
+##' Default is FALSE.  If \code{bw=TRUE}, parameters present in both
+##' models are plotted as grey points and parameters in only one model
+##' are plotted as unfilled circles.
+##' @param pointcol Specifies the point colour for parameters present
+##' in both models.  Default is \code{"#0072B2"} (a shade of blue), or
+##' \code{"grey50"} when \code{bw=TRUE}.
+##' @param altcol Specifies the point colour for parameters present in
+##' only one model.  Default is \code{"#E69F00"} (a shade of orange)
+##' when \code{bw=FALSE}.  When \code{bw=TRUE}, this parameter
+##' controls the colour of the point \emph{outline}, and the default
+##' value is \code{"black"}.
 ##' @return A \pkg{ggplot2}-package \code{ggplot} object.
 ##' @author Alexander Zwart (alec.zwart at csiro.au)
 ##' @export
 ##' @import ggplot2
 ##' @import asreml
-##' @import dplyr
 ##'
 replot <- function(curr,prev=NULL,gammas=TRUE,size=6,lwd=2,
-                          tritanopia=FALSE,zline=TRUE,rlines=TRUE)
+                   zline=TRUE,rlines=TRUE,
+                   bw=FALSE,
+                   pointcol=ifelse(bw,"grey50","#0072B2"),
+                   altcol=ifelse(bw,"black","#E69F00"))
   {
     stopifnot(is.numeric(size))
     stopifnot(is.numeric(lwd))
@@ -134,7 +143,7 @@ replot <- function(curr,prev=NULL,gammas=TRUE,size=6,lwd=2,
     ## model - these will be needed later...
     cr_rv <- cr[grepl("!variance$",cr$Term),]
     ##
-    if (!is.null(prev))  # ..and, if prev model is specified...
+    if (!is.null(prev))  # ...and, if prev model is specified...
       {
         pr <- summary(prev)$varcomp[,c(1,2,5)]
         pr$constraint=as.character(pr$constraint)
@@ -171,9 +180,8 @@ replot <- function(curr,prev=NULL,gammas=TRUE,size=6,lwd=2,
     if (!is.null(added))
       {
         ## Re color argument: if there is no previous model, plot
-        ## points in blue ("#0072B2").  Else, plot in green
-        ## ("#009E73"), or orange ("#E69F00") for those with
-        ## tritanopia.
+        ## points in colour 'pointcol'.  Else, plot in colour
+        ## 'altcol'.
         added$Term <- factor(added$Term,levels=termLevs)
         plt <- plt + geom_point(aes_string(x=ifelse(gammas,
                                                "gamma",
@@ -182,9 +190,10 @@ replot <- function(curr,prev=NULL,gammas=TRUE,size=6,lwd=2,
                                 data=added,
                                 size=size,
                                 color=ifelse(is.null(prev),
-                                    "#0072B2",
-                                    ifelse(tritanopia,"#E69F00",
-                                           "#009E73")))
+                                    pointcol,altcol),
+                                fill="white", ## redundant unless bw=TRUE
+                                stroke=1.5, ## unused unless bw=TRUE
+                                shape=ifelse(!is.null(prev) & bw,21,16))
         ##
         if (any(added$constraint=="Boundary")) {
           added_b <- added[added$constraint=="Boundary",]
@@ -211,18 +220,19 @@ replot <- function(curr,prev=NULL,gammas=TRUE,size=6,lwd=2,
                                y="Term"),
                     data=removed,
                     size=size,
-                    color=ifelse(tritanopia,
-                        "#E69F00",
-                        "#009E73")) +
-        geom_point(aes_string(x=ifelse(gammas,
-                                  "gamma",
-                                  "component"),
-                              y="Term"),
-                   data=removed,
-                   size=size,
+                    color=altcol,
+                    fill="white", ## unused unless bw=TRUE
+                    stroke=1.5, ## unused unless bw=TRUE
+                    shape=ifelse(bw,21,16)) +
+                     geom_point(aes_string(x=ifelse(gammas,
+                                               "gamma",
+                                               "component"),
+                                           y="Term"),
+                                data=removed,
+                                size=size,
 ###                   shape="\u2715",
-                   shape=4,
-                   stroke=1.5)
+                                shape=4,
+                                stroke=1.5)
         ##
         if (any(removed$constraint=="Boundary")) {
           removed_b <- removed[removed$constraint=="Boundary",]
@@ -235,8 +245,8 @@ replot <- function(curr,prev=NULL,gammas=TRUE,size=6,lwd=2,
                                   shape="|",
                                   size=size*1.3)
           ## size=size*1.3, shape="|" is not ideal, but it will do for
-          ## now.  The unicode option is nicer, but it's not clear that
-          ## the symbol will be available to all users...
+          ## now.  The unicode option is nicer, but I'm not sure
+          ## whether the symbol will be available to all users?
         }
       }  ## End handling of 'removed'
     ##
@@ -260,7 +270,7 @@ replot <- function(curr,prev=NULL,gammas=TRUE,size=6,lwd=2,
                                              y="Term"),
                                   data=inboth,
                                   size=size,
-                                  color="#0072B2")
+                                  color=pointcol)
         ##
         if (any(inboth$constraint.x=="Boundary")) {
           inboth_b_x <- inboth[inboth$constraint.x=="Boundary",]
